77da9181646f46ea92a04fc753c314c9fa760ba4
ma <442026460@qq.com>
2020-10-21 10:26:50 +0800

    修改输出对象改成按需加载

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-import Test from './components/Test/Test.vue'
-import MainContainer from './components/MainContainer/MainContainer.vue'
+import Test from './components/Test/index'
+import MainContainer from './components/MainContainer/index'
 
-export default function (Vue) {
-  Vue.component(Test.name, Test)
-  Vue.component(MainContainer.name, MainContainer)
+export default {
+  install(Vue) {
+    Test(Vue)
+    MainContainer(Vue)
+  },
+  Test: {
+    install(Vue) {
+      Test(Vue)
+    }
+  },
+  MainContainer: {
+    install(Vue) {
+      MainContainer(Vue)
+    }
+  }
 }
